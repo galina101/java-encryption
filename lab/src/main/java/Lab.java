9a5b83cd8d467758d55a095a58c4cf3b745a4a8e
galina101 <goncharovagalinka@gmail.com>
2024-03-19 12:34:06 -0400

    my copy

--- lab/src/main/java/Lab.java
+++ lab/src/main/java/Lab.java
@@ -1,30 +1,19 @@
-import java.util.Arrays;
-
 public class Lab {
     /**
-     * An anagram is a word that is the reordering of another word's letters.
-     * Letter counts must be exact.
-     *
-     * @param s1 a word.
-     * @param s2 another word, possibly an anagram of s1.
-     * @return True if s2 is an anagram of s1, false otherwise.
+     * @param nums a list of ints.
+     * @return a list of ints that is in the reversed order of nums.
      */
-    public boolean check(String s1, String s2){
-        //If s1 and s2 are not of the same length,
-        // return false because anagrams must have the same number of letters.
-        if(s1.length() != s2.length()){
-            return false;
+    public int[] reverse(int[] nums){
+
+
+        int[] reversedArray = new int[nums.length];
+        int j = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            reversedArray[j] = nums[i];
+            j++;
         }
 
-        //Convert both s1 and s2 to character arrays
-        char[] s1Array = s1.toCharArray();
-        char[] s2Array = s2.toCharArray();
 
-        //Sort both character arrays
-        Arrays.sort(s1Array);
-        Arrays.sort(s2Array);
-
-        return Arrays.equals(s1Array, s2Array);
+        return reversedArray;
     }
-
 }
