4b7efa6e0bad89f3dd1a3147c83f19a47bb202c1
galina101 <goncharovagalinka@gmail.com>
2024-03-19 12:04:37 -0400

    second save

--- lab/src/main/java/Lab.java
+++ lab/src/main/java/Lab.java
@@ -10,17 +10,19 @@ public class Lab {
      * @return True if s2 is an anagram of s1, false otherwise.
      */
     public boolean check(String s1, String s2){
-
+        //If s1 and s2 are not of the same length,
+        // return false because anagrams must have the same number of letters.
         if(s1.length() != s2.length()){
             return false;
         }
 
+        //Convert both s1 and s2 to character arrays
         char[] s1Array = s1.toCharArray();
         char[] s2Array = s2.toCharArray();
 
+        //Sort both character arrays
         Arrays.sort(s1Array);
         Arrays.sort(s2Array);
-
 
         return Arrays.equals(s1Array, s2Array);
     }
